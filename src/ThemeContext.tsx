d516783e743cb9796ba716636ff4995dd34ada4c
wt-854 <wt854@uowmail.edu.au>
2020-06-03 22:52:21 +0800

    convert to typescript

---
+++ src/ThemeContext.tsx
@@ -0,0 +1,9 @@
+import { createContext, useState } from "react";
+
+const ThemeContext = createContext<[string, (theme: string)
+=> void]>([
+  "green", 
+  () => {}
+]);
+
+export default ThemeContext;
